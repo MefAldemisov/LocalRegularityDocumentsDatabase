505b0542b943923e528523118043c2b7f7ecb782
MefAldemisov <alisha-omg@yandex.ru>
2020-04-29 23:26:18 +0300

    feat: testing tunning started

---
+++ lrdb-front/tests/app.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from "@vue/test-utils";
+import tipContainer from "../src/components/tips/tip_container.vue";
+
+describe("TodoList", () => {
+    it("should render to a snapshot when tips list is empty", () => {
+        const propsData = { tips: ["search", "load"] };
+        const wrapper = shallowMount(tipContainer, { propsData });
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it("should render to a snapshot when tips list has items", () => {
+        const propsData = { tips: ["load", "search"] };
+        const wrapper = shallowMount(tipContainer, { propsData });
+        expect(wrapper.element).toMatchSnapshot();
+    });
+});
