251c3ef35b175e93d5f5302900b8328c14d2af9d
Pain122 <pavelx6@gmail.com>
2020-06-02 11:20:37 +0300

    Added document history to the project features (requires a bit osv improvement)s

--- lrdb_back/document_storage/urls.py
+++ lrdb_back/document_storage/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('owners/', views.OwnerList.as_view()),
     path('owners/<int:pk>/', views.OwnerDetails.as_view()),
     path('docowner/<str:pk>/', views.DocumentOwnerView.as_view()),
+    path('update/<int:pk>/', views.DocumentUpdate.as_view()),
+    path('update/', views.HistoryList.as_view()),
 ]
